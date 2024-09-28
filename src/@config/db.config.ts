a68eaf4fa82e1c614e4cb38cb7a75af799a848ff
sidor91 <sidorchik91@gmail.com>
2024-09-28 12:38:29 +0200

    ADD | DB + GQL configs

---
+++ src/@config/db.config.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_DB_ATLAS_URL'),
+      }),
+    }),
+  ],
+})
+export class DataBaseConfigModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DataBaseConfigModule } from './@config/db.config';
+import { GraphQLConfigModule } from './@config/gql.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DataBaseConfigModule,
+    GraphQLConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
