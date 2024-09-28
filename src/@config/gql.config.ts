87eafb7cac6d5f420e48ad4b55bf971ddce73929
sidor91 <sidorchik91@gmail.com>
2024-09-28 15:55:35 +0200

    Update gql.config.ts
    autoSchemaFile: true

--- src/@config/gql.config.ts
+++ src/@config/gql.config.ts
@@ -1,13 +1,12 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: true,
       playground: true,
     }),
   ],
